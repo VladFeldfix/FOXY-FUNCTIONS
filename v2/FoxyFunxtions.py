8a35806d4247817d2162617d1b256c30a59f2c9d
Vlad Feldfix <62237119+VladFeldfix@users.noreply.github.com>
2022-11-08 15:16:34 +0200

    Update FoxyFunxtions.py

--- v2/FoxyFunxtions.py
+++ v2/FoxyFunxtions.py
@@ -3,6 +3,11 @@ import os
 """
 FOXY FUNCTIONS v1.0
 
+To add FoxyFunctions to your code put the file FoxyFunxtions.py in the same folder as your file and write: 
+    from FoxyFunxtions import ff
+Then initialize foxyfunctions as following 
+    ff("your program name", "your program version")
+
 function you can interact with:
 1. ff(program_name, program_ver) initialization
     when initializing foxy-functions specify the name and revision of you app
@@ -11,14 +16,17 @@ function you can interact with:
     the header is the name of the menu (for example: MAIN MENU, SETTINGS ect...)
     the menu is a list of tuples that specify the menu text and the function it is activating. (for example: [('f', f), ('g', g)])
 
-3. display_line(line, leng)
+3. display_header()
+    displays the name and verstion of your program + the version of foxy functions you are using at the moment. no arguments needed
+    
+4. display_line(line, leng)
     for example: line="hello world", leng=20 hello_world_________ where _ is an empty space not underscore
-
-4. error(text)
-    displays an error message
 
 5. exit()
     finishes the program
+    
+6. error(text)
+    displays an error message
 """
 
 class ff():
@@ -26,24 +34,10 @@ class ff():
         self.version = "1.0"
         self.program_name = program_name
         self.program_ver = str(program_ver)
-        self.display_header()
-    
-    # public
-    def display_line(self, line, leng):
-        spaces = leng - len(line)
-        return line+" "*spaces
-    
-    # private
-    def display_header(self):
-        display_expression = "-- "+self.program_name+" v"+self.program_ver+" ff"+self.version+" --"
-        print("-"*len(display_expression))
-        print(display_expression)
-        print("-"*len(display_expression))
 
     # header for example: MAIN MENU, menu structure exmple: [('start', start), ('edit', edit), ('exit', exit)]
     def display_menu(self, header, menu):
         os.system('cls') # clear screen
-        self.display_header()
         print(header)
         i = 0
         
@@ -64,14 +58,18 @@ class ff():
             self.internal_error("INVALID INPUT")
             self.display_menu(header, menu)
     
-    # public fnnction
+    def display_header(self):
+        display_expression = "-- "+self.program_name+" v"+self.program_ver+" ff"+self.version+" --"
+        print("-"*len(display_expression))
+        print(display_expression)
+        print("-"*len(display_expression))
+        
+    def display_line(self, line, leng):
+        spaces = leng - len(line)
+        return line+" "*spaces
+      
     def exit():
         input("DONE >")
     
-    # public fnnction
-    def error(text):
-        input("ERROR! "+text+" >")
-
-    # private function
-    def internal_error(self, text):
+    def error(self, text):
         input("ERROR! "+text+" >")
